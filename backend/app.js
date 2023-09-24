fd225d4405e0ebee5680cadedee9ab31e455e674
Piyush Gupta <guptapiyush315@gmail.com>
2023-09-24 22:53:09 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
+app.use("/api/v1", order);
+
 
 
 // Middleware for Errors
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -23,5 +23,11 @@ router.route("/admin/product/:id").put(isAuthenticatedUser, authorizeRoles("admi
 
 router.route("/product/:id").get(getProductDetails);
 
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router
+    .route("/reviews")
+    .get(getProductReviews)
+    .delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
