af9e438e1225419ca1e56d61cbcf4899797e0f70
Piyush Gupta <guptapiyush315@gmail.com>
2023-09-28 17:55:21 +0530

    ongoing

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+
+  
+  }, []);
+
   return (
-    <div>
-        
-    </div>
+    <Router>
+        <Header/>
+        <Route exact path="/" component={Home} />
+        <Footer/>
+    </Router>
   );
 }
 
